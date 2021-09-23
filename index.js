931687ea5ea4933d819cf0a93319b90922dc2630
Connor Cantrell <connorjcantrell@gmail.com>
2021-09-23 07:21:16 -0700

    Renamed variables

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ const COLUMNS = 16
 
 const rootEl = document.querySelector('#root')
 
-function generateGrid(rows, columns) {
-    let gridEl = document.createElement('div')
-    gridEl.setAttribute('id', 'grid')
-    gridEl.style.display = 'grid'
-    gridEl.style.gridTemplate = `${ROWS} ${COLUMNS} auto`
+function generateArtboard(rows, columns) {
+    let artboardEl = document.createElement('div')
+    artboardEl.setAttribute('id', 'artboard')
+    artboardEl.style.display = 'artboard'
+    artboardEl.style.artboardTemplate = `${ROWS} ${COLUMNS} auto`
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < columns; col++) {
             let id = row * ROWS + col
             let squareDiv = document.createElement('div')
             squareDiv.setAttribute('class', 'square')
             squareDiv.setAttribute('id', `${id}`)
-            squareDiv.style.gridRow = `${row + 1}`
-            squareDiv.style.gridColumn = `${col + 1}`
+            squareDiv.style.artboardRow = `${row + 1}`
+            squareDiv.style.artboardColumn = `${col + 1}`
             buttonEl = document.createElement('button')
             buttonEl.addEventListener('mouseover', function() {
                 squareDiv.style.background = 'black'
             })
             squareDiv.appendChild(buttonEl)
-            gridEl.appendChild(squareDiv)
+            artboardEl.appendChild(squareDiv)
         }
     }
-    return gridEl
+    return artboardEl
 } 
 
 function main() {
-    let grid = generateGrid(ROWS, COLUMNS)
-    rootEl.appendChild(grid)
+    let artboard = generateArtboard(ROWS, COLUMNS)
+    rootEl.appendChild(artboard)
 }
 
 main()
