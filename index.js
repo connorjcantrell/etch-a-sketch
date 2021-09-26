12aed0b91fa565c45a2765a6e830ba0ad5e37391
Connor Cantrell <connorjcantrell@gmail.com>
2021-09-26 14:55:13 -0700

    Implementing event bubbling

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const ROWS = 16
 const COLUMNS = 16
 
+const bodyEl = document.querySelector('body')
 const rootEl = document.querySelector('#root')
 const sliderEl = document.querySelector('#slider')
 const resetButtonEl = document.querySelector('#reset')
 const rainbowInputEl = document.querySelector('#rainbow')
+const squareDivs = document.querySelector('.square')
+const color = 'black'
 
 
-function generateGrid(rows, columns, color) {
+function generateGrid(rows, columns) {
     let gridEl = document.createElement('div')
     gridEl.setAttribute('id', 'grid')
     gridEl.style.gridTemplateColumns = `${rows} ${columns} 1fr 1fr`
@@ -19,13 +22,6 @@ function generateGrid(rows, columns, color) {
             squareDiv.setAttribute('id', `${id}`)
             squareDiv.style.gridRow = `${row + 1}`
             squareDiv.style.gridColumn = `${col + 1}`
-            squareDiv.addEventListener('mouseover', function() {
-                if (color === 'rainbow') {
-                    squareDiv.style.backgroundColor = `rgb(${random255()},${random255()},${random255()}, 1)`
-                } else {
-                    squareDiv.style.backgroundColor = color
-                }
-            })
             gridEl.appendChild(squareDiv)
         }
     }
@@ -37,20 +33,26 @@ function random255() {
 }
 
 function main() {
-    let grid = generateGrid(ROWS, COLUMNS, 'black')
+    bodyEl.addEventListener('mouseover', event => {
+        if (event.target === squareDivs) {
+            let squareDiv = document.querySelector(`#${event.target.id}`)
+            if (rainbowInputEl.checked) {
+                squareDiv.style.backgroundColor = `rgb(${random255()},${random255()},${random255()}, 1)`
+            } else {
+                squareDiv.style.backgroundColor = 'black'
+            }
+        }
+    })
+    let grid = generateGrid(ROWS, COLUMNS)
     rootEl.appendChild(grid)
     resetButtonEl.addEventListener('click', function() {
-        let color = 'black'
-        if (rainbowInputEl.checked === true) {
-            color = 'rainbow'
-        }
         // delete all contents in root element
         while (rootEl.firstChild) {
             rootEl.removeChild(rootEl.firstChild)
         }
         // generate new grid with updated parameters
         let resolution = sliderEl.value
-        grid = generateGrid(resolution, resolution, color)
+        grid = generateGrid(resolution, resolution)
         rootEl.appendChild(grid)
     })
 }
